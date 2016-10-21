c4287abaaed7b42d05e127efc36725f4bd59cc2b
André Luís Bordignon <albordignon@gmail.com>
2016-10-21 16:28:03 -0200

    Inicio da exibição dos votos no mapa

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -4,7 +4,7 @@ angular.module('mapavotosApp')
 
     .controller('MapaController', ['$scope', 'mapaFactory', function($scope, mapaFactory){
         
-        var mymap = L.map('mapid').setView([-22.905556, -47.060833], 13);
+        var mymap = L.map('mapid').setView([-22.905556, -47.060833], 12);
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
             maxZoom: 20,
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
@@ -17,21 +17,55 @@ angular.module('mapavotosApp')
         $scope.votos = mapaFactory.getMapa();
         $scope.vereadores = mapaFactory.getVereadores();
         // *** vereador que será selecionado.
-        $scope.verSelected = "xxx";
+        $scope.verSelected = "";
         $scope.latitude = -22.905556;
+        $scope.longitude = -22.905556;
+        $scope.raio = 0;
         
         $scope.selVereador = function(){  
-            $scope.latitude = $scope.latitude + 0.000003;
+            /*$scope.latitude = $scope.latitude + 0.000003;
             var circle = L.circle([$scope.latitude, -47.060833], 500, {
     		color: 'red',
     		fillColor: '#f03',
     		fillOpacity: 0.5
-			}).addTo(mymap);        
+			}).addTo(mymap);    */
+            
+            var i = 0;   
+            var marker;
+            var circle;
+            for(i=0; i<$scope.votos.length; i++){
+                console.log($scope.votos[i].local);
+                $scope.latitude = parseFloat($scope.votos[i].latitude);
+                $scope.longitude = parseFloat($scope.votos[i].longitude);
+                //marker = L.marker([$scope.latitude, $scope.longitude]).addTo(mymap);
+            
+                //$scope.raio = eval("$scope.votos["+i.toString()+"].$scope." + $scope.verSelected.toString());
+                
+                    
+                $scope.raio = eval("$scope.votos["+i+"]"+".n13001");
+                            
+                console.log($scope.raio);
+                
+                
+                /*circle = L.circle([$scope.latitude, $scope.longitude], $scope.raio, {
+    		      color: 'red',
+    		      fillColor: '#f03',
+    		      fillOpacity: 0.5
+			     }).addTo(mymap);*/
+                
+            }
+                
+
+            
         };
         
-        $scope.counter = 0;
-        $scope.change = function() {
-            $scope.counter++;
-        };
+        
+        
+        
+        
+        
+        
+        
+        
             
     }]);
