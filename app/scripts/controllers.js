e2d2b530ff52c77b1783ede48c76b819df470620
André Luís Bordignon <albordignon@gmail.com>
2016-12-01 17:29:18 -0200

    Adicionado o contorno em Campinas

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -4,7 +4,8 @@ angular.module('mapavotosApp')
 
     .controller('MapaController', ['$scope', 'mapaFactory', function($scope, mapaFactory){
         
-        var mymap = L.map('mapid').setView([-22.905556, -47.060833], 12);
+        var mymap = L.map('mapid').setView([-22.905556, -47.060833], 11);
+
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
             maxZoom: 20,
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
@@ -12,8 +13,22 @@ angular.module('mapavotosApp')
                 'Imagery © <a href="http://mapbox.com">Mapbox</a>',
             id: 'mapbox.streets'
         }).addTo(mymap); 
+
+        //Desenha os limites de Campinas na tela. Ref.:http://leafletjs.com/examples/geojson/
+        $scope.coordCamp = mapaFactory.getCoordCamp();
+        var myStyle = {
+                "color": "#ff7800",
+                "weight": 1,
+                "opacity": 0.15
+            };
+        L.geoJSON($scope.coordCamp, {
+                style: myStyle
+            }).addTo(mymap);
+        
+        //--------------------------------------------
         
         $scope.message = "Mapa de Votos de Campinas";
+        $scope.coordCamp = mapaFactory.getCoordCamp();
         $scope.votos = mapaFactory.getMapa();
         $scope.vereadores = mapaFactory.getVereadores();
         // *** vereador que será selecionado.
@@ -24,7 +39,7 @@ angular.module('mapavotosApp')
         $scope.marker = [];
         $scope.circle = [];
         $scope.mostraescolas = true;
-        
+        var i;
         $scope.selVereador = function(){  
             /*$scope.latitude = $scope.latitude + 0.000003;
             var circle = L.circle([$scope.latitude, -47.060833], 500, {
@@ -33,7 +48,7 @@ angular.module('mapavotosApp')
     		fillOpacity: 0.5
 			}).addTo(mymap);    */
 
-            for(var i=0; i<$scope.votos.length; i++){
+            for(i=0; i<$scope.votos.length; i++){
                 $scope.latitude = parseFloat($scope.votos[i].latitude);
                 $scope.longitude = parseFloat($scope.votos[i].longitude);
                 $scope.marker[i] = L.marker([$scope.latitude, $scope.longitude]).addTo(mymap);
@@ -59,7 +74,7 @@ angular.module('mapavotosApp')
                 mymap.removeLayer($scope.circle[i]);
             }
             
-        }
+        };
         
         $scope.Escolas = function(){
             
@@ -70,7 +85,7 @@ angular.module('mapavotosApp')
                 } 
                 $scope.mostraescolas = false;
             } else{
-                for(var i=0; i<$scope.votos.length; i++){
+                for(i=0; i<$scope.votos.length; i++){
                     $scope.latitude = parseFloat($scope.votos[i].latitude);
                     $scope.longitude = parseFloat($scope.votos[i].longitude);
                     $scope.marker[i] = L.marker([$scope.latitude, $scope.longitude]).addTo(mymap);
@@ -78,6 +93,6 @@ angular.module('mapavotosApp')
                 $scope.mostraescolas = true;
             }
             
-        }
+        };
             
     }]);
