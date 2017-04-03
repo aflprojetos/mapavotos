ba53cd91538a56a9e750c692dd516f22ab105979
André Luís Bordignon <albordignon@gmail.com>
2017-04-03 15:04:37 -0300

    Atualizado código do mapbox

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -6,7 +6,7 @@ angular.module('mapavotosApp')
         
         var mymap = L.map('mapid').setView([-22.905556, -47.060833], 11);
 
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWxib3JkaWdub24iLCJhIjoiY2l0MzFlZXdzMHRyNjJvcGdnY2txZXdsMCJ9.IOAXYE1_mrHHHUbVMxR59Q', {
             maxZoom: 20,
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
                 '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
